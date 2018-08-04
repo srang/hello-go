490e18186587c29d61344f1d7e369d8f5f5d6f71
Sam Rang <srang2010@gmail.com>
2018-08-04 14:02:50 -0400

    .

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", handler) // each request calls handler
-	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 // handler echoes the Path component of the requested URL.
